7656e8112cd8ab65e705e7db52a6762faf11bf1a
Mingyu Kang <plenilune095@gmail.com>
2019-10-08 00:00:21 +0900

    사칙연산자 이해하기(+,-,*,/,**)

--- 020.py
+++ 020.py
@@ -3,8 +3,16 @@ b = 4
 ret1 = a+b
 ret2 = a-b              #ret1 = 2+4 = 6
 ret3 = a*b              #ret2 = 2-4 = -2
-ret4 = a/b              #ret3 = 2/4 = 0/5
+ret4 = a/b              #ret3 = 2/4 = 0.5
 ret5 = a**b             #ret5 = 2**4 = 16
 ret6 = a+a*b/a          #ret6 = 2+2*4/2 = 6
 ret7 = (a+b)*(a-b)      #ret7 = (2+4)*(2-4) = -12
 ret8 = a*b**a           #ret8 = 2*4**2 = 32
+print(ret1)
+print(ret2)
+print(ret3)
+print(ret4)
+print(ret5)
+print(ret6)
+print(ret7)
+print(ret8)
